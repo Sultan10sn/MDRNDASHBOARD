cdfe2f1a85b059ab6d60e9f92df642871ff10732
sultan10sn <sultan10sn@gmail.com>
2023-05-03 15:29:14 +0100

    added ScrollToTop component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import { HideSidebar, NavBar, Sidebar } from "./components";
+import { HideSidebar, NavBar, ScrollToTop, Sidebar } from "./components";
 import { Dashboard, HandleReport, Listview, Login } from "./pages";
 
 
@@ -8,22 +8,23 @@ function App() {
   return (
     <>
       <Router>
-        <div className="flex">
-          <HideSidebar>
-            <Sidebar />
-          </HideSidebar>
-          <main className="w-full">
-            <NavBar />
+        <ScrollToTop>
+          <div className="flex">
+            <HideSidebar>
+              <Sidebar />
+            </HideSidebar>
+            <main className="w-full">
+              <NavBar />
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/table" element={<Listview />} />
+                <Route path="/handlereport/:id" element={<HandleReport />} />
+                <Route path="/login" element={<Login />} />
+              </Routes>
 
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/table" element={<Listview />} />
-              <Route path="/handlereport/:id" element={<HandleReport />} />
-              <Route path="/login" element={<Login />} />
-            </Routes>
-
-          </main>
-        </div>
+            </main>
+          </div>
+        </ScrollToTop>
       </Router>
     </>
   )
